86d5757bdf2c64c7d82d6321e4745da12131cf69
Hugo Klok <hugo.klok@coolblue.nl>
2022-12-11 16:57:36 +0100

    Replace hardcoded username with env var

--- src/utils/loadGames.ts
+++ src/utils/loadGames.ts
@@ -1,8 +1,9 @@
 // import type { ChessComGame } from "../types/ChessComAPI";
+import { env } from "../env/server.mjs";
 
 export const loadGames = () => {
   const loadedGames = fetch(
-    "https://api.chess.com/pub/player/kaasboom/games/2022/12"
+    `https://api.chess.com/pub/player/${env.PLAYER_USERNAME}/games/2022/12`
   ).then((res) => res.json());
 
   return loadedGames;

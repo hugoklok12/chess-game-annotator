f24f55e8ee5f0ca1c7998075957e17ccc767ba23
Hugo Klok <hugo.klok@coolblue.nl>
2022-12-11 13:21:11 +0100

    Add refresh button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -17,10 +17,11 @@ const Home: NextPage = () => {
       </Head>
       <div className="my-20 mx-auto h-4/6 max-w-7xl rounded bg-black">
         <header className="border-b border-b-gray-400 p-5">
-          <nav className="flex w-full flex-row gap-4">
+          <nav className="flex w-full flex-row justify-between gap-4">
             <Link className="text-white" href={"/"}>
               Chess Game Annotator
             </Link>
+            <button className="text-white">Refresh games</button>
           </nav>
         </header>
         <main className="p-5">
